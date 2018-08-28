c569818fe5fb3642821a14866406c56a1b18ad60
Steven Huang <steven.charles.huang@gmail.com>
2018-08-28 01:26:59 -0400

    fetching data from googleapis, printing formatted_address, lat and lng

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const request = require('request');
+const logger = require('morgan');
 
 request({
   url: 'http://maps.googleapis.com/maps/api/geocode/json?address=220%20sinclair%20ave%20staten%20island',
   json: true
 }, (error, response, body) => {
-  console.log(body);
+  console.log(JSON.stringify(response, undefined, 2));
+  console.log(`Address: ${body.results[0].formatted_address}`);
+  console.log(`Location Lat: ${body.results[0].geometry.location.lat}`);
+  console.log(`Location Lat: ${body.results[0].geometry.location.lng}`);
 });
