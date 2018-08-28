ad8e0dc60f1b902b277a512283cd684a4b20e2a1
Steven Huang <steven.charles.huang@gmail.com>
2018-08-28 02:23:35 -0400

    added forecast.io fetching, added env handling for api key

--- app.js
+++ app.js
@@ -1,26 +1,38 @@
+require('dotenv').config();
 const app     = require('express')();
+const request = require('request');
 const geocode = require('./geocode/geocode');
 const logger  = require('morgan');
 const yargs   = require('yargs');
 
-const argv = yargs
-  .options({
-    a: {
-      demand:   true,
-      alias:    'address',
-      describe: 'Address to fetch weather for',
-      string:   true
-    }
-  })
-  .help()
-  .alias('help', 'h')
-  .argv;
+// const argv = yargs
+//   .options({
+//     a: {
+//       demand:   true,
+//       alias:    'address',
+//       describe: 'Address to fetch weather for',
+//       string:   true
+//     }
+//   })
+//   .help()
+//   .alias('help', 'h')
+//   .argv;
 
-geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-  errorMessage ? console.log(errorMessage) : console.log(JSON.stringify(results, undefined, 2));
-});
+// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+//   errorMessage ? console.log(errorMessage) : console.log(JSON.stringify(results, undefined, 2));
+// });
 
-console.log(argv);
+// console.log(argv);
 
 app.use(logger('dev'));
 
+request({
+  url: `https://api.darksky.net/forecast/${process.env.SECRET_KEY}/42.3601,-71.0589`,
+  json: true,
+}, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    console.log(body.currently.temperature);
+  } else {
+    console.log('Unable to fetch weather');
+  }
+});
