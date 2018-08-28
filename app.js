d6c3c8f9afd637ac8c7c2b27847ad2eeabeead2a
Steven Huang <steven.charles.huang@gmail.com>
2018-08-28 02:40:57 -0400

    refactored app.js, separated concern for weather fetching

--- app.js
+++ app.js
@@ -2,37 +2,43 @@ require('dotenv').config();
 const app     = require('express')();
 const request = require('request');
 const geocode = require('./geocode/geocode');
+const weather = require('./weather/weather');
 const logger  = require('morgan');
 const yargs   = require('yargs');
 
-// const argv = yargs
-//   .options({
-//     a: {
-//       demand:   true,
-//       alias:    'address',
-//       describe: 'Address to fetch weather for',
-//       string:   true
-//     }
-//   })
-//   .help()
-//   .alias('help', 'h')
-//   .argv;
+app.use(logger('dev'));
+const argv = yargs
+  .options({
+    a: {
+      demand:   true,
+      alias:    'address',
+      describe: 'Address to fetch weather for',
+      string:   true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
 
-// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-//   errorMessage ? console.log(errorMessage) : console.log(JSON.stringify(results, undefined, 2));
-// });
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+  if (errorMessage) {
+    console.log(errorMessage)
+  } else {
+    console.log(results.address);
+    weather.getWeather(results.latitude, results.longitude, (errorMessage, weatherResults) => {
+      if (errorMessage) {
+        console.log(errorMessage);
+      } else {
+        console.log(`
+          It's currently ${weatherResults.temperature}.
+          It feels like ${weatherResults.apparentTemperature}`
+        );
+      }
+    });
+  };
+});
 
 // console.log(argv);
 
-app.use(logger('dev'));
 
-request({
-  url: `https://api.darksky.net/forecast/${process.env.SECRET_KEY}/42.3601,-71.0589`,
-  json: true,
-}, (error, response, body) => {
-  if (!error && response.statusCode === 200) {
-    console.log(body.currently.temperature);
-  } else {
-    console.log('Unable to fetch weather');
-  }
-});
+
