b0163c2ebb94113a93ca889ebf9bd6d7dbc3b9c8
Steven Huang <steven.charles.huang@gmail.com>
2018-08-28 01:49:00 -0400

    added basic error handling

--- app.js
+++ app.js
@@ -26,8 +26,14 @@ request({
   url: `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
   json: true
 }, (error, response, body) => {
-  console.log(JSON.stringify(response, undefined, 2));
-  console.log(`Address: ${body.results[0].formatted_address}`);
-  console.log(`Location Lat: ${body.results[0].geometry.location.lat}`);
-  console.log(`Location Lat: ${body.results[0].geometry.location.lng}`);
+  if (error) {
+    console.log('Unable to connect to Google servers');
+  } else if (body.status === 'ZERO_RESULTS') {
+    console.log('Unable to find that adderess');
+  } else if (body.status === 'OK') {
+    console.log(JSON.stringify(response, undefined, 2));
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Location Lat: ${body.results[0].geometry.location.lat}`);
+    console.log(`Location Lat: ${body.results[0].geometry.location.lng}`);
+  }
 });
